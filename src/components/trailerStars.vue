26559885eb7968032e255ec1231fde75f625aa25
qinxiaomeng <qinxiaomeng.happy@gmail.com>
2019-12-23 23:42:55 +0800

    首页 done

---
+++ src/components/trailerStars.vue
@@ -0,0 +1,61 @@
+<template name="trailerStars">
+	<view class="movie-score-wapper">
+		<image v-for="yellow in yellowScore" src="../static/icos/star-yellow.png" class="star-ico"></image>
+		<image v-for="gray in grayScore" src="../static/icos/star-gray.png" class="star-ico"></image>
+		<view class="movie-score" v-if="showNum == 1">
+			{{innerScore}}
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "trailerStars",
+		props: {
+			innerScore: {
+				type: Number,
+				default: 0
+			},
+			showNum: {
+				type: Number,
+				default: 0
+			}
+		},
+		data() {
+			return {
+				yellowScore: 0,
+				grayScore: 5
+			};
+		},
+		created() {
+			let tempScore = 0;
+			if (this.innerScore != null && this.innerScore != undefined) {
+				tempScore = this.innerScore > 10 ? 10 : this.innerScore;
+			}
+
+			let yellowScore = parseInt(tempScore / 2);
+			let grayScore = 5 - yellowScore;
+			this.yellowScore = yellowScore;
+			this.grayScore = grayScore;
+		}
+	}
+</script>
+
+<style>
+	.movie-score-wapper {
+		display: flex;
+		flex-direction: row;
+	}
+
+	.star-ico {
+		width: 20rpx;
+		height: 20rpx;
+		margin-top: 6rpx;
+	}
+
+	.movie-score {
+		font-size: 12px;
+		color: grey;
+		margin-left: 8rpx;
+	}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,6 @@
 <template>
 	<view class="page">
+		
 		<!-- 轮播图 start -->
 		<swiper :indicator-dots="true" :autoplay="true" class="carousel">
 			<swiper-item v-for="carousel in carouselList" :key="carousel.id">
@@ -23,41 +24,138 @@
 						<image :src="superhero.cover" class="poster"></image>
 					</view>
 					<view class="movie-name">{{superhero.name}}</view>
-					<view class="movie-score-wapper">
-						<image src="../../static/icos/star-yellow.png" class="star-ico"></image>
-						<image src="../../static/icos/star-yellow.png" class="star-ico"></image>
-						<image src="../../static/icos/star-yellow.png" class="star-ico"></image>
-						<image src="../../static/icos/star-yellow.png" class="star-ico"></image>
-						<image src="../../static/icos/star-gray.png" class="star-ico"></image>
-						<view class="movie-score">
-							9.1
-						</view>
-					</view>
+					<trailer-stars :innerScore="superhero.score" :showNum="1"></trailer-stars>
 				</view>
 			</view>
 			
 		</scroll-view>
 		<!-- 热门超英 -->
+		
+		<!-- 热门预告 -->
+		<view class="page-block super-hot">
+			<view class="hot-title-wapper">
+				<image src="../../static/icos/interest.png" class="hot-ico"></image>
+				<view class="hot-title">
+					热门预告
+				</view>
+			</view>
+		</view>
+		
+		<view class="page-block hot-movies">
+			<video
+				class="hot-movies-single"
+				v-for="trailer in hotTrailerList"
+				:poster="trailer.poster"
+				:src="trailer.trailer" controls></video>
+		</view>
+		<!-- 热门预告 -->
+		<!-- 猜你喜欢 -->
+		<view class="page-block super-hot">
+			<view class="hot-title-wapper">
+				<image src="../../static/icos/guess-u-like.png" class="hot-ico"></image>
+				<view class="hot-title">
+					猜你喜欢
+				</view>
+			</view>
+		</view>
+		
+		<view class="page-block guess-u-like">
+			<view class="single-like-movie" v-for="(gul,gIndex) in guessULikeList">
+				<image :src="gul.cover" class="like-movie"></image>
+				<view class="like-desc">
+					<view class="like-title">{{gul.name}}</view>
+					<trailer-stars :innerScore="gul.score"></trailer-stars>
+					<view class="like-info">{{gul.basicInfo}}</view>
+					<view class="like-info">{{gul.releaseDate}}</view>
+				</view>
+				<view class="movie-oper" :data-gIndex="gIndex" @click="praiseMe">
+					<image src="../../static/icos/praise.png" class="praise-ico"></image>
+					<view class="praise-me">赞一下</view>
+					<view :animation="animationDataArr[gIndex]" class="praise-me animation-opacity">+1</view>
+				</view>
+			</view>
+			
+		</view>
+		<!-- 猜你喜欢 -->
 	</view>
 </template>
 
 <script>
 	import common from "../../common/common.js"
+	import trailerStars from "../../components/trailerStars.vue"
 	export default {
+		components:{
+			trailerStars
+		},
 		data() {
 			return {
 				carouselList: [],
-				hotSuperheroList: []
+				hotSuperheroList: [],
+				hotTrailerList: [],
+				guessULikeList: [],
+				animationData: {},
+				animationDataArr: [{},{},{},{},{}]
 			}
 		},
-		onLoad() {
-
-		},
 		methods: {
-
+			praiseMe: function(e){
+				// #ifdef APP-PLUS || MP-WEIXIN
+				let index = e.currentTarget.dataset.gindex;
+				// console.log(index)
+				// return;
+				
+				//构建动画数据，并且通过step表示这组动画的完成
+				this.animation.translateY(-60).opacity(1).step({duration: 400});
+				
+				//导出动画数据到view组件，实现组件的动画效果
+				this.animationData = this.animation;
+				this.animationDataArr[index] = this.animationData.export();
+				//还原动画
+				setTimeout(function() {
+					this.animation.translateY(0).opacity(0).step({duration: 0});
+					this.animationData = this.animation;
+					this.animationDataArr[index] = this.animationData.export();
+				}.bind(this), 500);
+				// #endif
+			},
+			refresh: function(){
+				let that = this;
+				let serverUrl = common.serverUrl;
+				uni.showLoading({
+					mask:true
+				})
+				//查询猜你喜欢
+				uni.request({
+					url: serverUrl + '/index/guessULike?qq=280555437',
+					method: 'POST',
+					success: res => {
+						if (res.data.status == 200) {
+							let guessULikeList = res.data.data;
+							that.guessULikeList = guessULikeList;
+						}
+					},
+					fail: () => {},
+					complete: () => {
+						uni.hideLoading();
+						uni.stopPullDownRefresh();
+					}
+				});
+			}
+		},
+		onPullDownRefresh(){
+			this.refresh();
+		},
+		onUnload() {
+			//页面卸载的时候清除动画数据
+			this.animationData = {}
 		},
 		onLoad() {
 			let that = this;
+			// #ifdef APP-PLUS || MP-WEIXIN
+			//在页面创建的时候创建一个临时动画
+			this.animation = uni.createAnimation();
+			// #endif
+			
 			let serverUrl = common.serverUrl;
 			uni.request({
 				url: serverUrl + '/index/carousel/list?qq=280555437',
@@ -69,7 +167,7 @@
 					}
 				}
 			});
-			
+			//热门超英
 			uni.request({
 				url: serverUrl + '/index/movie/hot?qq=280555437&type=superhero',
 				method: 'POST',
@@ -82,6 +180,23 @@
 				fail: () => {},
 				complete: () => {}
 			});
+			
+			
+			//视频
+			uni.request({
+				url: serverUrl + '/index/movie/hot?qq=280555437&type=trailer',
+				method: 'POST',
+				success: res => {
+					if (res.data.status == 200) {
+						let hotTrailerList = res.data.data;
+						that.hotTrailerList = hotTrailerList;
+					}
+				},
+				fail: () => {},
+				complete: () => {}
+			});
+			
+			that.refresh();
 		}
 	}
 </script>
